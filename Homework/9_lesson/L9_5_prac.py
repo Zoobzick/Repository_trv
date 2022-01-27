c76069f44a14b21193cf7a1ee9f1301827d06b92
Troover <murdoc-94@mail.ru>
2022-01-27 17:06:30 +0300

    Homework for 9th lesson added

---
+++ Homework/9_lesson/L9_5_prac.py
@@ -0,0 +1,44 @@
+class Stationery:
+    title: str
+
+    def draw(self) -> None:
+        print("Запуск отрисовки")
+
+
+class Pen(Stationery):
+    def __init__(self) -> None:
+        super().__init__()
+        self.title = "ручка"
+
+    def draw(self) -> None:
+        print("Пишем текст")
+        return None
+
+
+class Pencil(Stationery):
+    def __init__(self) -> None:
+        super().__init__()
+        self.title = "карандаш"
+
+    def draw(self) -> None:
+        print("Чертим чертеж")
+        return None
+
+
+class Handle(Stationery):
+    def __init__(self) -> None:
+        super().__init__()
+        self.title = "маркер"
+
+    def draw(self) -> None:
+        print("Выделяем заголовки")
+        return None
+
+pen = Pen()
+pencil = Pencil()
+handle = Handle()
+
+
+pen.draw()
+pencil.draw()
+handle.draw()
