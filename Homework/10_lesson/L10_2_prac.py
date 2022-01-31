552f062dd94fa437c5f7ceac0fb99357cb10da7c
Troover <murdoc-94@mail.ru>
2022-01-31 19:38:56 +0300

    Homework for 10 lesson added

---
+++ Homework/10_lesson/L10_2_prac.py
@@ -0,0 +1,87 @@
+from abc import ABC, abstractmethod
+
+
+class MaterialLenSolver:
+    __length: float = 0
+    __coat_list = []
+    __costume_list = []
+
+    @property
+    def length(self):
+        return self.__length
+
+    @property
+    def node_list(self):
+        return {"Coats": self.__coat_list, "Costumes": self.__costume_list}
+
+    @node_list.setter
+    def node_list(self, other, add=True):
+        if other.__class__ is Coat:
+            if add:
+                self.__coat_list.append(other.get_name())
+            else:
+                self.__coat_list.remove(other.get_name())
+        elif other.__class__ is Costume:
+            if add:
+                self.__costume_list.append(other.get_name())
+            else:
+                self.__costume_list.remove(other.get_name())
+        else:
+            raise ValueError("Unknown cloth type")
+
+    @length.setter
+    def length(self, in_len: float):
+        self.__length += in_len
+
+    def __iadd__(self, other):
+        self.length = other.material_length()
+        self.node_list = other
+        return self
+
+    def __isub__(self, other):
+        self.length = - other.material_length()
+        if other.__class__ is Coat and other.get_name() in self.__costume_list:
+            self.__costume_list.remove(other.get_name())
+        elif other.__class__ is Costume and other.get_name() in self.__coat_list:
+            self.__coat_list.remove(other.get_name())
+        return self
+
+
+class ACloth(ABC):
+    name: str
+
+    def get_name(self):
+        return self.name
+
+    @abstractmethod
+    def material_length(self):
+        """ returns length of material """
+
+
+class Coat(ACloth):
+
+    def __init__(self, name, size) -> None:
+        super().__init__()
+        self.name = name
+        self.size = size
+
+    def material_length(self):
+        return self.size * 6.5 + 0.5
+
+
+class Costume(ACloth):
+
+    def __init__(self, name, height) -> None:
+        super().__init__()
+        self.name = name
+        self.height = height
+
+    def material_length(self):
+        return 2 * self.height + 0.3
+
+
+coat = Coat("ZXC", 56)
+print(coat.material_length())
+
+costume = Costume('ABC', 179)
+print(costume.material_length())
